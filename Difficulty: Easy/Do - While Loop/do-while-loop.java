f96cee6c7ed695823482f0afe7223365f8e9ccfc
Sakshi Kumari <mishra0108sakshi@gmail.com>
2026-07-05 21:24:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Do - While Loop/do-while-loop.java
@@ -0,0 +1,9 @@
+class Solution {
+    void printGeeks(int n) {
+        int i = 0;
+        do {
+            System.out.println("Geeksforgeeks");
+            i++;
+        } while (i < n);
+    }
+}
